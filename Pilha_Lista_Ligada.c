4a56e58d7252ab8cfba82241a7d837bcd2f30d21
Matheus Capanema <71179413+MatheusRCapanema@users.noreply.github.com>
2022-04-04 20:09:44 -0300

    Update Pilha_Lista_Ligada.c

--- Pilha_Lista_Ligada.c
+++ Pilha_Lista_Ligada.c
@@ -28,7 +28,7 @@ registro *aloca_registro(){
     novo->prox=NULL;
     return novo;
 }
-void adicionar(lista *l,int numero){
+void push(lista *l, int numero){
     registro *novo;
     novo=aloca_registro();
     novo->valor =numero;
@@ -40,7 +40,14 @@ void adicionar(lista *l,int numero){
     }
     l->qtd++;
 }
-void mostrar_lista(lista *l){
+int empty(lista *l){
+    if(l->qtd==0){
+        return 0;
+    }else{
+        return 1;
+    }
+}
+/*void mostrar_pilha(lista *l){
     registro *aux;
     if(l->inicio==NULL){
         printf("\n Lista Vazia \n");
@@ -51,22 +58,22 @@ void mostrar_lista(lista *l){
             aux=aux->prox;
         }
     }
-}
-int buscar(lista *l,int numero){
+}*/
+/*int buscar(lista *l,int numero){
     if(l->inicio==NULL) {
         return 0;
     }else{
         registro *aux;
         aux=l->inicio;
         while(aux!=NULL){
-        if(aux->valor==numero){
-            return 1;
-        }
-        aux=aux->prox;
+            if(aux->valor==numero){
+                return 1;
+            }
+            aux=aux->prox;
         }
     }
-}
-int remover(lista *l,int numero){
+}*/
+int pop(lista *l, int numero){
     if(l->inicio==NULL) {
         return 0;
     }else{
@@ -88,15 +95,21 @@ int remover(lista *l,int numero){
         }
     }
 }
+int stackpop(lista *l){
+    if(empty(l)){
+        return 0;
+    }else{
+        return l->inicio->valor;
+    }
+}
 
 void menu(lista *l){
     int opcao;
     int numero;
     do{
-        printf("\n 01 - Incluir no inicio");
-        printf("\n 02 - Mostrar lista");
-        printf("\n 03 - Buscar numero");
-        printf("\n 04 - Remover numero");
+        printf("\n 01 - Incluir na pilha.");
+        printf("\n 02 - Mostrar topo");
+        printf("\n 03 - Remover numero na pilha");
         printf("\n 10 - Sair\n");
         scanf("%d", &opcao);
 
@@ -105,34 +118,22 @@ void menu(lista *l){
             case 1:
                 printf("\n Digite um numero:");
                 scanf("%d", &numero);
-                adicionar(l, numero);
+                push(l, numero);
                 break;
             case 2:
-                mostrar_lista(l);
+                stackpop(l);
+                printf("%d", l->inicio->valor);
                 break;
             case 3:
                 printf("\n Digite um numero: ");
-                scanf("%d", &numero);
-
-                if (buscar(l, numero))
-                {
-                    printf("\n Numero encontrado");
-                }
-                else
-                {
-                    printf("\n Numero nao esta na lista");
-                }
-                break;
-            case 4:
-                printf("\n Digite um numero: ");
                 scanf("%d",&numero);
-                if (remover(l,numero))
+                if (pop(l, numero))
                 {
                     printf("\n Numero removido ");
                 }
                 else
                 {
-                    printf("\n Numero nao removido ou lista vazia");
+                    printf("\n Numero nao removido ou pilha vazia");
                 }
                 break;
             case 10:
@@ -145,10 +146,10 @@ void menu(lista *l){
 
     } while (opcao != 10);
 }
-    int main(){
-        lista *l1;
-        l1=aloca_lista();
-        menu(l1);
-        return 0;
-    }
-
+int main(){
+    lista *l1;
+    l1=aloca_lista();
+    menu(l1);
+    return 0;
+}
+//Partes comentadas usadas apenas para testa.
