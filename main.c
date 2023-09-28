5927dab48acd094d22223e2c8994c61b74cac386
Matheus Capanema <71179413+MatheusRCapanema@users.noreply.github.com>
2023-09-28 16:19:15 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,96 +2,52 @@
 #include <stdbool.h>
 #include <string.h>
 
-# define SIZE 10000
+#define TAMANHO 10000
 
-int inp_arr[SIZE];
-int Rear = - 1;
-int Front = - 1;
+bool grafo[TAMANHO][TAMANHO];
+int distancia[TAMANHO];
+int n, t, m, x, y;
 
+int fila[TAMANHO];
+int inicio = -1, fim = -1;
 
-bool graph[SIZE][SIZE], found;
-int distancia[SIZE];
-int n, e, t, n, m, x, y;
+void bfs(int start) {
+    fila[++fim] = start;
+    distancia[start] = 0;
 
-void bfs(int node) {
-        if(distancia[node] == -1){
-            distancia[node] = 0;
-            for (int j = 1; j <= n ; ++j) {
-                if(graph[node][j]){
-                    if(distancia[j] == -1){
-                        distancia[j] = distancia[node] + 1;
-                    }
-                }
+    while (inicio != fim) {
+        inicio = (inicio + 1) % TAMANHO;
+        int nodo = fila[inicio];
+        for (int i = 1; i <= n; i++) {
+            if (grafo[nodo][i] && distancia[i] == -1) {
+                distancia[i] = distancia[nodo] + 1;
+                fim = (fim + 1) % TAMANHO;
+                fila[fim] = i;
             }
-        }else{
-            distancia[node] = distancia[node] + 1;
-
-            for(int i = 1; i<=n; i++){
-                if(graph[node][i]){
-                    if(distancia[i] == -1){
-                        distancia[i] = distancia[node];
-                    }
-                }
-            }
-
         }
-
+    }
 }
 
-
 int main() {
-
-    int maximo = 0;
-
     scanf("%d", &t);
     for (int cs = 1; cs <= t; cs++) {
         scanf("%d%d", &n, &m);
-
-        memset(graph, 0, sizeof graph);
+        memset(grafo, 0, sizeof grafo);
         memset(distancia, -1, sizeof distancia);
 
         for (int i = 0; i < m; i++) {
             scanf("%d%d", &x, &y);
-            graph[x][y] = graph[y][x] = 1;  // Marcar a aresta no gráfico
+            grafo[x][y] = grafo[y][x] = 1;
         }
 
-        for (int i = 1; i <= n; i++) {
-                bfs(i);  // Iniciar DFS com a primeira cor
+        bfs(1);
+
+        int maximo = 0;
+        for (int j = 1; j <= n; j++) {
+            maximo = (distancia[j] > maximo) ? distancia[j] : maximo;
         }
 
-        for (int j = 0; j <= n; ++j) {
-            if(distancia[n-1] < distancia[n]){
-                maximo = distancia[n];
-            }
-        }
-
-        printf("%d:\n", maximo);
-
+        printf("%d\n", maximo);
     }
-
+    return 0;
 }
-/*
- 3
-4 3
-1 2
-2 3
-3 4
-5 4
-1 2
-1 3
-3 4
-4 5
-6 7
-1 2
-1 3
-1 4
-2 5
-3 6
-4 6
-5 6
-
-
- 3
-2
-2
- */
